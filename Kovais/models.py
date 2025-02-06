56e147c3417a4b356bef78e5e478deefb7d60027
gokulrajalp <gokulrajalp@gmail.com>
2025-02-06 16:25:18 +0530

    Reset to 6e4db488f7d06a52a168f8000cdaffc2428dedd4

--- Kovais/models.py
+++ Kovais/models.py
@@ -5,53 +5,53 @@ from django.contrib.auth.models import AbstractUser
 class Employee(AbstractUser):
     ROLE_CHOICES = (
         ('admin', 'Admin'),
-        ('member','Member'),
-        ('saloon','Saloon'),
-        ('spa','Spa'),
-        ('gym','Gym'),
-        ('hotel','Hotel')
+        ('member', 'Member'),
+        ('saloon', 'Saloon'),
+        ('spa', 'Spa'),
+        ('gym', 'Gym'),
+        ('hotel', 'Hotel')
     )
- 
+
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='member')
-    mobile=models.CharField(max_length=255,null=True,blank=True)
-    location = models.CharField(max_length=255,blank=True,null=True)
+    mobile = models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
     password = models.CharField(max_length=255)
-    attendance= models.CharField(max_length=15,null=True,blank=True)
-    total_attendance=models.CharField(max_length=255,blank=True,null=True)
-    image=models.CharField(max_length=255,blank=True,null=True)
+    attendance = models.CharField(max_length=15, null=True, blank=True)
+    total_attendance = models.CharField(max_length=255, blank=True, null=True)
+    image = models.CharField(max_length=255, blank=True, null=True)
     is_active = models.BooleanField(default=True)
-    success =models.BooleanField(default=False)
+    success = models.BooleanField(default=False)
     groups = models.ManyToManyField(
         'auth.Group',
         verbose_name='groups',
         blank=True,
-        related_name='Employee',  # Unique related name for groups
-        related_query_name='Employee',
+        related_name='employees',  # Unique related name for groups
+        related_query_name='employee',
     )
     user_permissions = models.ManyToManyField(
         'auth.Permission',
         verbose_name='user permissions',
         blank=True,
-        related_name='Employee',  # Unique related name for user permissions
-        related_query_name='Employee',
-    )  
-  
+        related_name='employees',  # Unique related name for user permissions
+        related_query_name='employee',
+    )
 
-    
 class UserDetails(models.Model):
     MEMBERSHIP_CHOICES = (
-        ('silver','Silver'),
-        ('gold','Gold'),
-        ('platinum','Platinum'),
+        ('silver', 'Silver'),
+        ('gold', 'Gold'),
+        ('platinum', 'Platinum'),
     )
-    name= models.CharField(max_length=256,blank=True,null=True)
+    name = models.CharField(max_length=256, blank=True, null=True)
     membership = models.CharField(max_length=20, choices=MEMBERSHIP_CHOICES, default='silver')
     password = models.CharField(max_length=255)
-    subscribed=models.BooleanField(default=False)
-    premium_amount =models.CharField(max_length=255)
+    subscribed = models.BooleanField(default=False)
+    premium_amount = models.CharField(max_length=255)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
+  
+
     
 class Bonus(models.Model):
     name =models.ForeignKey(Employee,on_delete=models.CASCADE)
@@ -81,6 +81,7 @@ class SaloonOrder(models.Model):
     
 
 class GymOrder(models.Model):
+    customer_id=models.ForeignKey(UserDetails, on_delete=models.CASCADE,null=True,blank=True)
     gender=models.CharField(max_length=255,null=True,blank=True)
     age = models.CharField(max_length=255,null=True,blank=True)
     timeslot=models.CharField(max_length=255,null=True,blank=True)
@@ -90,6 +91,7 @@ class GymOrder(models.Model):
     category=models.CharField(max_length=255,null=True,blank=True)
     amount=models.TextField(null=True,blank=True)
     purchaseddate=models.TextField(null=True,blank=True)
+    expiry_date = models.TextField(null=True,blank=True)
     created_at =models.DateTimeField(auto_now_add=True)
 
 
@@ -100,6 +102,10 @@ class SpaOrder(models.Model):
     services=models.TextField(null=True,blank=True)
     date=models.DateField(null=True,blank=True)
     time=models.CharField(max_length=255,null=True,blank=True)
+    amount=models.TextField(null=True,blank=True)
+    payment_status=models.CharField(max_length=255,null=True,blank=True)
+    payment_type=models.CharField(max_length=255,null=True,blank=True)
+
     created_at =models.DateTimeField(auto_now_add=True)
 
 class HotelOrder(models.Model):
@@ -110,3 +116,16 @@ class HotelOrder(models.Model):
     date=models.DateField(null=True,blank=True)
     time=models.CharField(max_length=255,null=True,blank=True)
     created_at =models.DateTimeField(auto_now_add=True)
+
+class Attendance(models.Model):
+    employee_attendance = models.ForeignKey(
+        Employee,
+        on_delete=models.CASCADE,
+        related_name='attendances'  # Unique related name for reverse relation
+    )
+    status = models.CharField(max_length=255)
+    check_in = models.DateTimeField(null=True, blank=True)
+    check_out = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.employee_attendance} - Status: {self.status}'

669eeb5180c3215f37c5af4ac9ca68689f671432
maanickavasagar <manikvasu2000@gmail.com>
2025-02-18 01:11:46 +0530

    fix:changed capture and kovais new update

--- building_construction/settings.py
+++ building_construction/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-qy99_@e#dcu^cbt$=dazit$_*)5yz*)f_6cmkpxu9r-a9)iq90
 # DEBUG = True
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','api.capture360.ai', 'b5f2-59-97-51-97.ngrok-free.app','192.168.1.55','192.168.1.36','59.97.51.97','192.168.1.33','c432-59-97-51-97.ngrok-free.app','capture360.ai','www.capture360.9ai',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','api.capture360.ai', 'fd84-59-97-51-97.ngrok-free.app','192.168.1.55','192.168.1.36','59.97.51.97','192.168.1.33','c432-59-97-51-97.ngrok-free.app','capture360.ai','www.capture360.9ai',]
 
 CORS_ALLOW_HEADERS = [
     'Accept',
@@ -57,7 +57,7 @@ CORS_ALLOW_HEADERS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'https://b5f2-59-97-51-97.ngrok-free.app',
+    'https://fd84-59-97-51-97.ngrok-free.app',
     'http://192.168.1.33:8080',
     'https://api.capture360.ai',
     'http://api.capture360.ai',
@@ -122,7 +122,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+#     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'building_construction.urls'

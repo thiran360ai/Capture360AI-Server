9b5dcbbcdb1f8bc2b8e1e82e799dad1483c35e4f
maanickavasagar <manikvasu2000@gmail.com>
2025-03-25 17:33:23 +0530

    new api

--- officeapp/urls.py
+++ officeapp/urls.py
@@ -8,6 +8,19 @@ urlpatterns = [
         path('create-user/', create_user, name='create_user'),
         path('login/', login_view, name='login'),
         path('create-employee/', CreateEmployeeView.as_view(), name='create_employee'),
+        
+        path('list-user-request/', InactiveEmployeesView.as_view(), name='inactive-employees'),
+        path('list-device-id-request/', ActiveDevicesView.as_view(), name='devices'),
+     
+        path('user/status/', CheckUserActiveStatusView.as_view(), name='user-status'),
+        path('Approve-User/', UpdateUserActiveStatusView.as_view(), name='user-update-status'),
+        path('Approve-Device-ID/', UpdateDeviceActiveStatusView.as_view(), name='device-update-status'),
+        path('check-user-approve-status/', CheckEmployeeStatusView.as_view(), name='check-employee-status'),
+        path('check-device-id-approve-status/', CheckDeviceStatusView.as_view(), name='check-device-status'),
+
+        path('attendance/total-hours/', GetTotalHoursView.as_view(), name='get-total-hours'),
+
+
         path('inactive-employees/<str:organization_key>/', InactiveEmployeeListView.as_view(), name='inactive-employees'),
         path('update-employee-status/', UpdateEmployeeStatusView.as_view(), name='update-employee-status'),
         path('employee-login/', EmployeeLoginView.as_view(), name='employee-login'),

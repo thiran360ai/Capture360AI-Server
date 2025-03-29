a626d449f2982199c1352c5b16dbdd49d55c5bbc
veera <ramanavalar2001@gmail.com>
2025-03-29 16:58:49 +0530

    fix:resolve models issue

--- gateway/serializers.py
+++ gateway/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Device, GPSData
+from .models import Device, GPSData,CustomUser
 
 User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ["id", "username", "email", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = CustomUser.objects.create_user(**validated_data)
         return user
 
 class DeviceSerializer(serializers.ModelSerializer):

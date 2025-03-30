09d00bb3e1fd118a25d4fe5db352dc4a3c6a50df
veera <ramanavalar2001@gmail.com>
2025-03-30 21:05:22 +0530

    Fix: changes in login function in IOT

--- gateway/urls.py
+++ gateway/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import create_user, create_device, create_gps_data, get_gps_data,login_view
+from .views import create_user, create_device, create_gps_data, get_gps_data,login_user,login_view
 
 urlpatterns = [
     path("user/create/", create_user, name="create-user"),
     path("device/create/", create_device, name="create-device"),
     path("gpsdata/create/", create_gps_data, name="create-gpsdata"),
     path("gpsdata/<str:device_id>/", get_gps_data, name="get-gpsdata"),
-    path("login/", login_view, name="login"),
+    path("login/", login_user, name="login"),
 ]

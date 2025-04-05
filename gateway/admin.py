e0bc872a41c912b9aad753414f27b851cca2c4d5
Thamodharans <thamotenyson001@gamil.com>
2025-04-05 12:44:27 +0530

    complete

---
+++ gateway/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Device, GPSData
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):  # Extend UserAdmin for better UI
+    list_display = ("username", "email",  "password")
+    search_fields = ("username", "email")
+    ordering = ("email",)
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("user", "device")
+
+
+@admin.register(GPSData)
+class GPSDataAdmin(admin.ModelAdmin):
+    list_display = ("device", "latitude", "longitude", "timestamp")
+

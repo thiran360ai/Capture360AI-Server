a7eb1ba61d4a74ad91467781fc50cfe75245856c
maanickavasagar <manikvasu2000@gmail.com>
2025-04-17 09:40:28 +0530

    new update for model

--- gateway/models.py
+++ gateway/models.py
@@ -29,10 +29,10 @@ class CustomUser(AbstractUser):
 class Device(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="devices")
     device = models.CharField(max_length=100, unique=True)
-    name = models.CharField(max_length=255, null=True, blank=True)  # ✅ this line
+    name = models.CharField(max_length=255, null=True, blank=True)  
 
     def __str__(self):
-        return f"{self.name} ({self.device})"
+        return self.device
 
 
 
